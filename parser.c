b85344be24728b578302c23a2cd72fa7dd2f08f0
Adam Levy <Alevy01@gmail.com>
2015-11-07 14:45:39 -0500

    Initial commit of parser

---
+++ parser.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef enum {
+     nulsym = 1, identsym, numbersym, plussym, minussym,
+     multsym,  slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
+     gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+     periodsym, becomessym, beginsym, endsym, ifsym, thensym,
+     whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+     readsym, elsesym
+} token_type;
+
+
+
+int main(){
+
+
+
+	return 0;
+}
+
