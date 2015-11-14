4bf348b80f29cfa8399627354dade9d3cec91f4f
Sagar <sagar.mistry@knights.ucf.edu>
2015-11-13 22:08:08 -0500

    added break statements to void error()

--- parser.c
+++ parser.c
@@ -107,62 +107,88 @@ void getToken(FILE *ifp){
 
 
 void error(int errNumber){
-    /*
+    
      switch(errNumber){
      case 1:
      printf("Use = instead of :=\n");
+     break;
      case 2:
      printf("= must be followed by a number\n");
+     break;
      case 3:
      printf("Identifier must be folowed by :=\n");
+      break;
      case 4:
      printf("Const,Var,Procedure, must be followed by identifier\n");
+      break;
      case 5:
      printf("Semicolon or comma missing.\n");
+      break;
      case 6:
      printf("Incorrect symbol after procedure declaration.\n");
+      break;
      case 7:
      printf("Statement Expected.\n");
+      break;
      case 8:
      printf("Incorrect symbol after statement part in block.\n");
+      break;
      case 9:
      printf("Period expected.\n");
+      break;
      case 10:
      printf("Semicolon between statements missing.\n");
+      break;
      case 11:
      printf("Undeclared Identifier\n");
+      break;
      case 12:
      printf("Assignment to constatn or procedure is not allowed.\n");
+      break;
      case 13:
      printf("Assignment operator expected\n");
+      break;
      case 14:
      printf("Call must be followed by an identifier.\n");
+      break;
      case 15:
      printf("Call of a constant or variable is meaningless.\n");
+      break;
      case 16:
      printf("then expcted.\n");
+      break;
      case 17:
      printf("Semicolon or } expected.\n");
+      break;
      case 18:
      printf("do expected.\n");
+      break;
      case 19:
      printf("Incorrect symbol following statement.\n");
+      break;
      case 20:
      printf("Relational operator expected.\n");
+      break;
      case 21:
      printf("Expression must not contain a procedure identifier.\n");
+      break;
      case 22:
      printf("Right Parenthesis is missing.\n");
+      break;
      case 23:
      printf("The preceding factor cannot begin with this symbol.\n");
+      break;
      case 24:
      printf("An expression canot begin with this symbol.\n");
+      break;
      case 25:
      printf("This number is too large.\n");
+      break;
      default:
      printf("Error. Not a valid code Error value.\n");
+      break;
      }
-     printf("done\n");*/
+     printf("done\n");
 }
 
 void block(FILE *ifp){
