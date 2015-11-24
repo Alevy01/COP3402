80574e8c9aeea80b1ffdb4d59f871fb5a66d2698
zackshackleton <minimallymint@gmail.com>
2015-11-23 19:40:40 -0500

    Better

--- myParser.c
+++ myParser.c
@@ -47,7 +47,6 @@ FILE *input;
 int currToken = 0;
 int sTableCount = 0;
 int curLexLevel = 0;
-int curLine = 1;
 int cx = 0;
 
 
@@ -77,15 +76,8 @@ int main(void) {
     
     printSymbolTable();
 
-    if (currToken != periodsym){
-        if(currToken != nulsym){
-            error(8);
-        }
-        else{
-            error(9);
-        }
-    }
-        
+    if (currToken != periodsym)
+        error(9);
     
     emit(11, 0, 3);
     
@@ -97,8 +89,6 @@ int main(void) {
 
 void getToken() {
     fscanf(input, "%d ", &currToken);
-    if (currToken == semicolonsym)
-        curLine++;
     return;
 }
 
@@ -198,7 +188,6 @@ void block() {
         
         getToken();
     }
-
     codeList[jmpCX].m = cx;
     
     emit(INC, 0, mCount);
@@ -258,9 +247,6 @@ void statement() {
             printf("\ntoken: %d\n", currToken);
             error(17);
         }
-        else if(currToken == beginsym || currToken == identsym){
-            error(10);
-        }
         
         getToken();
     }
@@ -313,56 +299,6 @@ void statement() {
         emit(JPC, 0, aCX);
         codeList[bCX].m = cx;
     }
-    else{
-        if(currToken == periodsym){
-            error(17);
-        }
-        else{
-            error(7);
-        }
-    }
-
-    //Writesym
-    else if (currToken == writesym) {
-        getToken();
-        if (currToken != identsym)
-            error(26);
-
-        char temp[12];
-        fscanf(input, "%s", temp);
-        int result = isInSymTable(temp);
-
-        if (symbolTable[result].kind == 1) 
-            emit(LIT, 0, symbolTable[result].value);
-
-        else if (symbolTable[result].kind == 2)
-            emit(LOD, curLexLevel - symbolTable[result].level, symbolTable[result].addr);
-
-        else 
-            error(11);
-
-        emit(SIO, 0, 0);
-        getToken();
-    }
-
-    //Readsym
-    else if (currToken == readsym) {
-        getToken();
-        if (currToken != identsym)
-            error(27);
-
-        char temp[12];
-        fscanf(input, "%s", temp);
-        int result = isInSymTable(temp);
-
-        if (symbolTable[result].kind != 2)
-            error(11);
-
-        emit(SIO, 0, 1);
-        emit(STO, curLexLevel - symbolTable[result].level, symbolTable[result].addr);
-
-        getToken();
-    }
 }
 
 
@@ -391,11 +327,6 @@ void condition() {
 void expression() {
     int addOP;
     
-    if(currToken != numbersym && currToken != identsym && currToken != lparentsym
-        && currToken != minussym && currToken != plussym){
-        error(24);
-    }
-
     if (currToken == plussym || currToken == minussym) {
         addOP = currToken;
         
@@ -505,7 +436,7 @@ int isInSymTable(char temp[12] ) {
 
 void error(int errNumber){
     
-    printf("\nError Code %d on line %d: ", errNumber, curLine);
+    printf("\nError Code %d on line %d: ", errNumber, cx);
     switch(errNumber){
         case 1:
             printf("Use = instead of :=\n");
@@ -525,23 +456,23 @@ void error(int errNumber){
         case 6:
             printf("Incorrect symbol after procedure declaration.\n");
             break;
-        case 7:
-            printf("Statement Expected.\n");
-            break;
-        case 8:
-            printf("Incorrect symbol after statement part in block.\n");
-            break;
+            // case 7:
+            //     printf("Statement Expected.\n");
+            //     break;
+            // case 8:
+            //     printf("Incorrect symbol after statement part in block.\n");
+            //     break;
         case 9:
             printf("Period expected.\n");
             break;
-        case 10:
-            printf("Semicolon between statements missing.\n");
-            break;
+            // case 10:
+            //     printf("Semicolon between statements missing.\n");
+            //     break;
         case 11:
             printf("Undeclared Identifier\n");
             break;
         case 12:
-            printf("Assignment to constant or procedure is not allowed.\n");
+            printf("Assignment to constatn or procedure is not allowed.\n");
             break;
         case 13:
             printf("Assignment operator expected\n");
@@ -549,9 +480,9 @@ void error(int errNumber){
         case 14:
             printf("Call must be followed by an identifier.\n");
             break;
-        // case 15:
-        //     printf("Call of a constant or variable is meaningless.\n");
-        //     break;
+            // case 15:
+            //     printf("Call of a constant or variable is meaningless.\n");
+            //     break;
         case 16:
             printf("then expcted.\n");
             break;
@@ -576,9 +507,9 @@ void error(int errNumber){
         case 23:
             printf("The preceding factor cannot begin with this symbol.\n");
             break;
-        case 24:
-            printf("An expression canot begin with this symbol.\n");
-            break;
+            // case 24:
+            //     printf("An expression cannot begin with this symbol.\n");
+            //     break;
         case 25:
             printf("This number is too large.\n");
             break;
